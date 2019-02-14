d35866c95e1bed7a28ca54634562d97c516e55ee
Dana Mastropole <Dana@Danas-MacBook-Pro-2.local>
2019-02-14 03:35:07 -0500

    add summary logs

--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python retrain.py --image_dir=./train_images --output_graph=./model/output_graph.pb --output_labels=./model/output_labels.txt --how_many_training_steps=2000
+python retrain.py --image_dir=./train_images --output_graph=./model/output_graph.pb --output_labels=./model/output_labels.txt --summaries_dir=./model/retrain_logs --how_many_training_steps=2000
