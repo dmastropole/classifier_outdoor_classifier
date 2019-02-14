dbbdc92c568662217aa4fc72bcdc3427fe4c5b38
Dana Mastropole <Dana@Danas-MacBook-Pro-2.local>
2019-02-14 03:15:25 -0500

    add model and modify bash script

--- predict.sh
+++ predict.sh
@@ -5,4 +5,4 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-python label_image.py --graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt --input_layer=Placeholder --output_layer=final_result --image=$1
+python label_image.py --graph=./model/output_graph.pb --labels=./model/output_labels.txt --input_layer=Placeholder --output_layer=final_result --image=$1
--- train.sh
+++ train.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python retrain.py --image_dir=./test_images
+python retrain.py --image_dir=./train_images --output_graph=./model/output_graph.pb --output_labels=./model/output_labels.txt --how_many_training_steps=2000
